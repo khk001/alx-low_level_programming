1de7794d04bbcf64e95560fed0be3c752e73eec6
KHK001 <khk.cfr001@gmail.com>
2023-06-15 15:28:54 +0100

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - printing a line using the puts function
  * return 0; - successful execution
  */
-int main() {
+int main(void) {
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
