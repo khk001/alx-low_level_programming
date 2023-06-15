cbbf2998b6859e6806a7f47651e8c800855d1ff2
Khalid Katou <khk.cfr001@gmail.com>
2023-06-15 07:25:40 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - printing a line using the puts function
+ * return 0; - successful execution
+ */
+int main() {
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
