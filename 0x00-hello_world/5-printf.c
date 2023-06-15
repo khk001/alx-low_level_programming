d6abf85768821dbdf0f1ecc206291d3529f93bab
Khalid Katou <khk.cfr001@gmail.com>
2023-06-15 07:47:53 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - printing a line using the printf function
+ *
+ * return: 0 for successful execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}	
