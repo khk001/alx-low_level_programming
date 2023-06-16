0216875462846e6eeb74f1c179aeb4b1802cbab1
KHK001 <khk.cfr001@gmail.com>
2023-06-16 10:24:30 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for successful execution
+ */
+int main(void)
+{
+int lowerCase = 97;
+int upperCase = 65
+while (lowerCase <= 122)
+{
+	putchar(num);
+	num++;
+}
+while (upperCase <= 90)
+{
+	putchar(num);
+	num++;
+}
+putchar('\n');
+return (0);
+}
