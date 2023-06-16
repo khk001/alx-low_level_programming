84250babde943aef5b2ea020500d5152c4fc5307
KHK001 <khk.cfr001@gmail.com>
2023-06-16 12:29:32 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for successful execution
+ */
+int main(void)
+{
+	int num;
+	int alph;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	for (alph = 97; alph <= 102; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n')
+	return (0);
+}
