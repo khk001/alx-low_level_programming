3bb1e993d24c5d4757aaa17892402f2c8687ae5a
KHK001 <khk.cfr001@gmail.com>
2023-06-16 22:08:33 +0100

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,21 +9,21 @@ int main(void)
 {
 	int d, p, q;
 
-	for(d = '0'; d < '9'; d++)
+	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + 1; p <= '9'; p++)
 		{
-		for(p = d + 1; p <= '9'; p++)	
-		{
-		for(q = p + 1; q <= '9'; q++)
-	    {
-		    if ((p != d) != q)
-		{
-				putchar(d);
-				putchar(p);
-				putchar(q);
-				if (d == '7' && p == '8')
-				continue;
-				putchar(',');
-				putchar(' ');
+			for (q = p + 1; q <= '9'; q++)
+			{
+				if ((p != d) != q)
+				{
+					putchar(d);
+					putchar(p);
+					putchar(q);
+					if (d == '7' && p == '8')
+						continue;
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
@@ -31,4 +31,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
