e32eb480ebd5b55882f6d921c37b9c0f897e78a8
KHK001 <khk.cfr001@gmail.com>
2023-06-16 13:37:10 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for successful execution
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			if (j != i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
+
