5e02684f0ee22d38be5d2b5eca34cfad1aa866ae
KHK001 <khk.cfr001@gmail.com>
2023-06-16 09:50:50 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for successful execution
+ */
+int main(void)
+{
+int num = 97;
+while (num <= 122)
+{
+	putchar(num);
+	num++;
+}
+putchar('\n')
+return (0);
+}
