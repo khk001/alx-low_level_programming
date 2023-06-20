9dfb9d1de6fbb22f21dd065c70f4a288d361ebce
KHK001 <khk.cfr001@gmail.com>
2023-06-20 20:13:27 +0100

    task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character..
+ * _islower - checks for lowercase character.
+ * @c: The character to be checked.
  *
  * Description: function that checks for lowercase character.
  *
