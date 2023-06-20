cf4be79c0448f43b1f314dbcc15668a75e2e6f31
KHK001 <khk.cfr001@gmail.com>
2023-06-20 14:26:35 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  _abs - function name
+ * @i: The integer to be checked.
+ *
+ * Description: function that computes the absolute value of an integer.
+ *
+ * Return: absolute value.
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+}
