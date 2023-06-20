b616e0d40b9cdd8ac6205716bb61cf0c102a8602
KHK001 <khk.cfr001@gmail.com>
2023-06-20 14:55:11 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  print_last_digit - function name
+ * @num: The integer to be checked.
+ *
+ * Description: function that prints the last digit of a number.
+ *
+ * Return: the value of the last digit.
+ */
+int print_last_digit(int num)
+{
+	int ld;
+
+	ld = num % 10;
+	if (ld < 0)
+	{
+		_putchar(-ld + '0');
+		return (-ld);
+	}
+	else
+	{
+		_pucthar(ld + '0');
+		return (ld);
+	}
+}
