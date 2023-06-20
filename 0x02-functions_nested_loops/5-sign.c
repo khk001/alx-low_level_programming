b43c7b781b0b541010bb096b7f6d5f5e99674f9d
KHK001 <khk.cfr001@gmail.com>
2023-06-20 13:53:42 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *  _isalpha - function name
+ * @n: The character to be checked.
+ *
+ * Description: function that prints the sign of a number.
+ *
+ * Return: 1 if positive, -1 if negative, 0 otherwise.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(45);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
