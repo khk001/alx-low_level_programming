7d67f507be6718354f37c12b86514def340a6929
KHK001 <khk.cfr001@gmail.com>
2023-06-20 17:13:10 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - function name.
+ * @a: to be added to b.
+ * @b : to be added to a.
+ *
+ * Description: function that adds two integers and returns the result.
+ *
+ * Return: sum of a and b.
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
