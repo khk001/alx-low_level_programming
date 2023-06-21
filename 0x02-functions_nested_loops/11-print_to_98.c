2b263cbdb40717b30110eef74a238a4feb786640
KHK001 <khk.cfr001@gmail.com>
2023-06-21 01:31:23 +0100

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,26 +9,24 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
-
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (int i = n; i <= 98; i++)
 		{
 			if (i != 98)
 				printf("%d, ", i);
-			else if (i == 98)
+			else
 				printf("%d\n", i);
 		}
-		else if (n >= 98)
+	}
+	else
+	{
+		for (int j = n; j >= 98; j--)
 		{
-			for (j = n; j >= 98; j--)
-			{
-				if (j != 98)
-					printf("%d, ", j);
-				else if (j == 98)
-					printf("%d\n", j);
-			}
+			if (j != 98)
+				printf("%d, ", j);
+			else
+				printf("%d\n", j);
 		}
 	}
 }
