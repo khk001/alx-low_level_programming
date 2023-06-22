537ceeb7a64c25f0fcc6521b6f8606978f373eca
KHK001 <khk.cfr001@gmail.com>
2023-06-22 12:22:10 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include  "main.h"
+
+/**
+ * mul - function name
+ * @a:  parametre to be checked
+ * @b:  parametre to be checked
+ *
+ * Description: function that multiplies two integers.
+ *
+ * Return: result of the multiplicattion
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
