8a10dfd461787c841d5002fb45a18baab2837005
KHK001 <khk.cfr001@gmail.com>
2023-06-26 15:14:51 +0100

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ * reset_to_98 - function that takes a pointer and return 98.
  * @n: parametre to be checked
  *
  * Return: n
