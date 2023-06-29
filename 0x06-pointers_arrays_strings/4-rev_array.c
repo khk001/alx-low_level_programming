dbad7a8bfe540b952463187b9ecbf30162dc4497
KHK001 <khk.cfr001@gmail.com>
2023-06-29 23:56:20 +0100

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -20,27 +19,3 @@ void reverse_array(int *a, int n)
 		a[n] = j;
 	}
 }
-=======
-#include "main.h"
-
-/**
- * reverse_array - function that reverses the content
- * of an array of integers.
- * @a: param
- * @n: param
- *
- * Return: void
- */
-void reverse_array(int *a, int n)
-{
-	int i;
-	int j;
-
-	for (i = 0, j = n - 1; i < j; i++, j--)
-	{
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
-	}
-}
->>>>>>> d99b2a7e9fb2772190d5373214bdef9fdd84cf29
