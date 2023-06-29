e5c0af1d5648b46fffe89cfd72a761411a4d773f
KHK001 <khk.cfr001@gmail.com>
2023-06-29 16:37:48 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer.
+ * @n: parameter
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+        unsigned int num;
+
+        num = n;
+
+        if (n < 0)
+        {
+                _putchar('-');
+                num = -n;
+        }
+
+        if (num / 10 != 0)
+        {
+                print_number(num / 10);
+        }
+        _putchar((n1 % 10) + '0');
+}
