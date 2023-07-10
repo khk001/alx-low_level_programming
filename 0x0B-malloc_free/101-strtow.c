3a3085a299ec7e7a7ea1b96a9c876b8a23d857ec
KHK001 <khk.cfr001@gmail.com>
2023-07-10 05:47:18 -0700

    task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * count_word - function to count the number of words in a string
+ * @s: parameter
+ *
+ * Return: w
+ */
+int count_word(char *s)
+{
+	int flag, count, wordCount;
+
+	flag = 0;
+	wordCount = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		if (s[count] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			wordCount++;
+		}
+	}
+
+	return (wordCount);
+}
+/**
+ * **strtow - function that splits a string into words.
+ * @str: parameter
+ *
+ * Return: matrix
+ */
+char **strtow(char *str)
+{
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	matrix[k] = NULL;
+
+	return (matrix);
+}
