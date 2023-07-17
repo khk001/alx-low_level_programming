4da54caa2c31ad53e118c99b86a3d8dd2b7c1063
KHK001 <khk.cfr001@gmail.com>
2023-07-17 14:40:01 +0100

    task 0 and 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,10 +3,9 @@
 
 /**
  * struct dog - a dog's basic info
- * @name: member
- * @age: member
- * @owner: member
- *
+ * @name: member char var
+ * @age: member float var
+ * @owner: member char var
  */
 struct dog
 {
@@ -14,5 +13,18 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 
 #endif
