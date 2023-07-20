129c01287d55fa4291aff8e26eee7b44e1fc47b7
KHK001 <khk.cfr001@gmail.com>
2023-07-20 04:18:19 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,12 +13,12 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 	unsigned int i, sum = 0;
 
-	va_start(ap, n);
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
+		sum += va_arg(args, int);
 
-	va_end(ap);
+	va_end(agrs);
 
 	return (sum);
 }
