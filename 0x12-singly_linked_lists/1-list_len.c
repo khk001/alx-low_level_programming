2f3b218860a0989bffdeda85c5ded6671c38118c
KHK001 <khk.cfr001@gmail.com>
2023-07-27 08:40:28 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list
+ * @h: paramter
+ *
+ * Return: num
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (num);
+}
